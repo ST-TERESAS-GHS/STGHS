b9f758edcb02a1c9df3dc0ded93aafe90b39ce69
ST-TERESAS-GHS <stghsndkm@gmail.com>
2025-01-24 00:22:24 +0530

    form.php

--- form.php
+++ form.php
@@ -1,26 +1,25 @@
 <?php
-$name = $_post['name'];
-$visitor_email = $-post['email'];
-$subject = $_post['subject'];
-$message = $-post['message'];
+$name = $_POST['name']; // Corrected from $_post to $_POST
+$visitor_email = $_POST['email']; // Corrected from $-post to $_POST
+$subject = $_POST['subject']; // Corrected from $_post to $_POST
+$message = $_POST['message']; // Corrected from $-post to $_POST
 
 $email_from = 'www.stghs.com';
 
 $email_subject = 'New Form Submission';
 
-$email_body = "User Name: $name.\n". 
-$email_body = "User Email: $visitor_email.\n". 
-$email_body = "Subject: $subject.\n". 
-$email_body = "User Message: $message.\n";
+$email_body = "User Name: $name.\n".
+              "User Email: $visitor_email.\n".
+              "Subject: $subject.\n".
+              "User Message: $message.\n";
 
 $to = 'stghsndkm@gmail.com';
 
 $headers = "From: $email_from \r\n";
 
-$headers .= "Replay-To: $visitor_email \r\n";
+$headers .= "Reply-To: $visitor_email \r\n"; // Corrected "Replay-To" to "Reply-To"
 
+mail($to, $email_subject, $email_body, $headers);
 
-mail($to,$email_subject,$email_body,$headers);
-
-header("Location:contact.html");
+header("Location: contact.html"); // Added space between Location: and the file name
 ?>
